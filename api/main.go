5420804bc2e1a392fa136919aa5cfd7be8ea8a7a
Pedro Tavares <pedro.tavares@squair.io>
2023-02-21 21:45:48 -0300

    removed any reference to the metrics service since I still need to check it a little bit more

--- api/main.go
+++ api/main.go
@@ -4,10 +4,10 @@ import (
 	"clean-architecture-golang/internal/config"
 	"clean-architecture-golang/internal/handler"
 	"clean-architecture-golang/internal/infrastructure"
-	"clean-architecture-golang/internal/metric"
 	"clean-architecture-golang/internal/middleware"
 	"clean-architecture-golang/internal/repository"
 	"clean-architecture-golang/internal/service"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 )
@@ -18,18 +18,13 @@ func main() {
 
 	userRepo := repository.NewPostgresUserRepository(db)
 	userService := service.NewUserService(userRepo)
-	metricService, err := metric.NewPrometheusService()
-	if err != nil {
-		log.Fatal().Err(err).Send()
-	}
 
 	r := gin.Default()
 	r.Use(middleware.LogErrors())
-	r.Use(middleware.Metrics(metricService))
 
 	handler.MakeUserHandler(r, userService)
 
-	err = r.Run(":" + appConfig.Port)
-	if err != nil {
+	if err := r.Run(":" + appConfig.Port); err != nil {
+		log.Fatal().Err(err).Send()
 	}
 }
